e34b6e1373059bc10eeb325eb53f391f2c9389af
Luan Naves <115196918+luan-naves@users.noreply.github.com>
2022-10-09 23:37:44 -0300

    Delete 1009.py

--- Python/Lista 1/1009.py
+++
@@ -1,9 +0,0 @@
-#https://www.beecrowd.com.br/judge/pt/runs/code/29347301#
-
-nVendedor=input()
-salarioF=float(input())
-totalV=float(input())
-
-bonus = float(totalV*(15/100))
-total = (salarioF+bonus)
-print("TOTAL = R$ %.2f" % total)
